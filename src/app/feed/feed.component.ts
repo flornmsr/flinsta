4c854577e34da8c4842e98019998da1117bd8b9d
flornmsr <38752536+flornmsr@users.noreply.github.com>
2025-02-24 19:28:54 +0100

    Update feed.component.ts

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -108,7 +108,7 @@ export class FeedComponent implements OnInit {
   }
 
   setCountdown() {
-    var countDown = new Date('2025-04-26T17:00:00.000Z').getTime();
+    var countDown = new Date('2025-04-24T17:20:00.000Z').getTime();
 
     setInterval(() => {
 
@@ -126,7 +126,7 @@ export class FeedComponent implements OnInit {
         var minutes = Math.floor((difference % hourMillis) / minMillis)
         var seconds = Math.floor(difference % minMillis / 1000);
 
-        this.countdown = `Noch ~${days} Tage`
+        this.countdown = `Noch ${days} Tage ${hours}:${minutes}:${seconds}`
 
         this.progress = Math.round((100-(difference/(105*dayMillis))*100)*100) / 100;
         // this.progress = 99
